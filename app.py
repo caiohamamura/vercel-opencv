9c87814bd6621b3da0749a4eff7e96c70365c54a
caiohamamura <caiohamamura@gmail.com>
2024-09-25 00:06:31 -0300

    Run model locally

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import cv2
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import os
+from ultralytics import YOLO
+import base64
+
+model = YOLO(f'best.pt')
 
 app = Flask(
     __name__,
@@ -21,9 +25,6 @@ origins = [
     "http://localhost:8080",
 ]
 
-# Roboflow configuration
-ROBOFLOW_API_KEY = os.environ.get("ROBOFLOW_API_KEY")
-ROBOFLOW_MODEL_URL = "https://detect.roboflow.com/fenologia-tcc/3"
 
 @app.route('/')
 def index():
@@ -38,21 +39,7 @@ def analyze_image():
     file = request.files['file']
     image = file.read()
 
-    # Send image to Roboflow API
-    response = requests.post(
-        ROBOFLOW_MODEL_URL,
-        files={"file": image},
-        params={"api_key": ROBOFLOW_API_KEY}
-    )
-
-    # Parse response
-    response_json = response.json()
-    if "predictions" not in response_json or len(response_json["predictions"]) == 0:
-        return {"error": "No predictions found"}
-
-    # Extract the first prediction
-    prediction = response_json["predictions"][0]
-
+    
     # Load the image using opencv
     file_bytes = np.asarray(bytearray(image), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
@@ -61,8 +48,8 @@ def analyze_image():
     mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.uint8)
     
     # Create a polygon from points for the mask
-    points = prediction["points"]
-    pts = np.array([[point["x"], point["y"]] for point in points], dtype=np.int32)
+    prediction = model.predict(img)
+    pts = np.array([[x, y] for (x, y) in zip(prediction[0].summary()[0]['segments']['x'], prediction[0].summary()[0]['segments']['y'])], dtype=np.int32)
     cv2.fillPoly(mask, [pts], 255)
     
     # Slice the image using the mask
@@ -73,18 +60,31 @@ def analyze_image():
     hsv_img = cv2.cvtColor(sliced_img, cv2.COLOR_RGB2HSV)
     
     # Create hue mask for values between 32 and 85
-    lower_hue = np.array([32, 10, 10])
-    upper_hue = np.array([85, 255, 255])
+    lower_hue = np.array([35, 10, 10])
+    upper_hue = np.array([90, 255, 255])
     hue_mask = cv2.inRange(hsv_img, lower_hue, upper_hue)
 
     # Calculate percentage of pixels matching the hue mask
     npoints_leaf = (hue_mask > 0).sum()
     percentage = npoints_leaf/npoints_crown * 100
 
+    # Sliced image to JPEG format
+    _, buffer = cv2.imencode('.jpg', sliced_img)
+    # Convert the buffer to a Base64 string
+    base64_string = base64.b64encode(buffer).decode('utf-8')
+    # Format the Base64 string to be used in HTML <img> tag
+    crown_base64 = f"data:image/jpeg;base64,{base64_string}"
     
+    # HSV masked image to JPEG format
+    hsv_masked_img = cv2.bitwise_and(sliced_img, sliced_img, mask=hue_mask)
+    _, buffer = cv2.imencode('.jpg', hsv_masked_img)
+    # Convert the buffer to a Base64 string
+    base64_string = base64.b64encode(buffer).decode('utf-8')
+    # Format the Base64 string to be used in HTML <img> tag
+    leaf_base64 = f"data:image/jpeg;base64,{base64_string}"
 
     # Return the percentage as JSON
-    return jsonify({"matching_percentage": percentage})
+    return jsonify({"matching_percentage": percentage, "crown_image": crown_base64, "leaf_image": leaf_base64})
 
 
 if __name__ == "__main__":
